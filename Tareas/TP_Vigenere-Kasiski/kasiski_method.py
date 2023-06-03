26a792d2d53b0acf76d291ac3c06ccd614c90f75
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-06-03 00:37:36 -0300

    distancias y repeticiones hechas, falta terminar las otras funcionalidades

--- Tareas/TP_Vigenere-Kasiski/kasiski_method.py
+++ Tareas/TP_Vigenere-Kasiski/kasiski_method.py
@@ -1,1 +1,23 @@
-# Metodo de Kasiski
+import re
+from collections import Counter
+
+def kasiski_method(cipher_text):
+    # Paso 1: Encontrar las repeticiones de 3 o más caracteres
+    pattern = r'(?=(\w{3,})).*?\1'
+    repetitions = re.findall(pattern, cipher_text)
+
+    # Paso 2: Calcular las distancias entre las repeticiones
+    distances = [repetitions[i+1:].index(repetitions[i]) + len(repetitions[i])
+                 for i, repetition in enumerate(repetitions[:-1])]
+    
+    # Paso 3: Encontrar los factores comunes de las distancias
+
+        # Logica para el paso 3
+
+    # Paso 4: Generar posibles longitudes de clave
+
+        # Logica para el paso 4
+
+    # Paso 5: Retornar las posibles longitudes de clave
+
+        # Logica para el paso 5
